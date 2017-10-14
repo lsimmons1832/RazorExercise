687c83ec6413ff3e5718185f209f44b49db8c2c3
Latasha Simmons <lsimmons1832@gmail.com>
2017-10-14 10:22:26 -0500

    save work

---
+++ FaceMeetRazor/Models/Records.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FaceMeetRazor.Models
+{
+    public class Records
+    {
+            string Artist { get; set; }
+            string Albumn { get; set; }
+            string Title { get; set; }
+            DateTime ReleaseDate { get; set; }
+            double Price { get; set; }
+    }
+}
